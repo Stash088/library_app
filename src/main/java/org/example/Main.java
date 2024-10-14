d449e952731269d5c55ea285bc36fc7df8618c2d
amir <amir040202@mail.ru>
2024-10-14 18:34:50 +0300

    add HealthCheckController

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,21 +3,20 @@ import io.javalin.Javalin;
 //import org.example.book.BookController;
 import io.javalin.apibuilder.ApiBuilder.*;
 import org.example.book.BookController;
+import org.example.utils.HealthCheckController;
 
 public class Main {
-
-
     public static void main(String[] args) {
         var app = Javalin.create(javalinConfig -> {
             javalinConfig.jetty.defaultPort = 7070;
             javalinConfig.useVirtualThreads = true;
                 })
                 .get("/", ctx -> ctx.result("Hello World"))
+                .get("/health", HealthCheckController::healthCheck)
                 .get("/books" ,BookController::GetAllBooks)
                 .get("/books/{id}", BookController::fetchByID)
                 .post("/books/create", BookController::createBook)
                 .patch("/books/{id}", BookController::updateBookByID)
                 .start(7070);
-
     }
 }
